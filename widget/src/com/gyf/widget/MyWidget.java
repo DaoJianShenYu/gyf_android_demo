03644742dc702dabbbcb0f5dc9859e6103d0617a
DaoJianShenYu <1023014360@qq.com>
2016-10-11 18:46:53 +0800

    creat:first time

---
+++ widget/src/com/gyf/widget/MyWidget.java
@@ -0,0 +1,12 @@
+package com.gyf.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * 桌面小控件的广播接收者
+ * @author Administrator
+ *
+ */
+public class MyWidget extends AppWidgetProvider {
+	
+}
